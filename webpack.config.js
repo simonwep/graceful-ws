edd3610afe5bbd5e195a4bfe23b55cbe699a451a
Simon Reinisch <simon@reinisch.io>
2019-10-05 00:46:49 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const {version} = require('./package');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'production',
+
+    entry: {
+        'fancy-websocket': './src/index.ts'
+    },
+
+    output: {
+        filename: '[name].min.js',
+        library: 'FancyWebsocket',
+        libraryExport: 'default',
+        libraryTarget: 'umd'
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    'ts-loader',
+                    'eslint-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            filename: `[name].min.js.map`
+        }),
+        new webpack.BannerPlugin({
+            banner: `Fancy Websocket ${version} MIT | https://github.com/Simonwep/fancy-websocket`
+        })
+    ]
+};
