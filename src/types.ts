58b2dbc2a3f5b38fbb33169a95de4545677c0951
Simon Reinisch <simon@reinisch.io>
2019-10-17 17:40:16 +0200

    Make all options except ws optional

--- src/types.ts
+++ src/types.ts
@@ -12,9 +12,9 @@ export type Options = {
 
     // Websocket related settings (https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/WebSocket)
     ws: WebsocketSettings;
-    com: Communication;
+    com?: Communication;
 
-    pingTimeout: number;
-    pingInterval: number;
-    retryInterval: number;
+    pingTimeout?: number;
+    pingInterval?: number;
+    retryInterval?: number;
 };
